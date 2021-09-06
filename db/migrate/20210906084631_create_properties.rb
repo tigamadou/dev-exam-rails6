b20b56181a1674c001f9be1e9ce86d64110c6a60
Amadou <amadou140589@gmail.com>
2021-09-06 09:57:27 +0100

    Property component actions and views added

---
+++ db/migrate/20210906084631_create_properties.rb
@@ -0,0 +1,13 @@
+class CreateProperties < ActiveRecord::Migration[6.0]
+  def change
+    create_table :properties do |t|
+      t.string :property
+      t.integer :rent
+      t.integer :building_age
+      t.text :address
+      t.text :remarks
+
+      t.timestamps
+    end
+  end
+end
